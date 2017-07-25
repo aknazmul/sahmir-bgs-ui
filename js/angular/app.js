98f21c5c502fb013697eedbf3224ceb3dd0ddb16
aknazmul <aknazmul@gmail.com>
2017-07-25 22:54:57 +0600

    scroll fixed

--- js/angular/app.js
+++ js/angular/app.js
@@ -1,9 +1,4 @@
 (function () {
     'use strict'; 
-    angular.module('sahmirbgsApp', ['ui.bootstrap', 'slickCarousel', 'bootstrapLightbox', 'ui.router', 'duScroll', 'matchMedia', 'ngAnimate']);
+    angular.module('sahmirbgsApp', [ 'slickCarousel', 'ui.router', 'duScroll', 'matchMedia', 'ngAnimate']);
 })();
-
-
-
-//Page scroll 
-// app.value('duScrollBottomSpy', true); 
